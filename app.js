de40202884f61c9b10c9432b80c9c373858614c8
Stanislav Ivashchenko <isn-88@ya.ru>
2021-11-30 10:09:08 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//alert(1)
+
+// 1 Переменные
+// variable
+
+
+// устарело
+// var name = 'Stanislav'
+
+const firstName = 'Stanislav'
+const lastName = 'Ivashchenko'
+const age = 33
+const isProgrammer = true
+
+let result = 55
+
+//  Мутирование
+// console.log('Имя: ' + firstName + ', возраст: ' + age)
+// console.log(age.toString())
+
+const enterName = prompt('Введите фалилию')
+alert(firstName + ' ' + enterName)
